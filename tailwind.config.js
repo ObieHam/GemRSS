0ea579d2ebc2ec39d96516f118ab87d20af86dff
ObieHam <165133135+ObieHam@users.noreply.github.com>
2025-12-24 20:27:47 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        indigo: {
-          50: '#f5f7ff',
-          600: '#4f46e5',
-          700: '#4338ca',
-        },
-      },
-    },
-  },
-  plugins: [],
-};
+  ]
+}
